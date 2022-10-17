fa17a908d32916ae2818d87bfc51b22bd51331fa
LHE1016 <bin101016@gmail.com>
2022-10-17 12:01:52 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useRef, useState } from 'react';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
+import './App.css'
 
 function App() {
+  const[todos, setTodos] = useState([
+    {
+      id:1,
+      text  : '운동하기',
+      checked: true,
+    },
+    {
+      id:2,
+      text  : '요리하기',
+      checked: true,
+    },
+    {
+      id:3,
+      text  : '학원하기',
+      checked: false,
+    }
+  ]);
+
+  const nextId = useRef(4); // ref를 사용하여 변수 담기
+  const onInsert = useCallback(value =>{
+    const todo = {
+      id: nextId.current,
+      text: value,
+      checked: false,
+    }
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  },[todos]); // todos값이 바뀔때 마다
+
+  const onRemove = useCallback(id =>{
+    setTodos(todos.filter(todo => todo.id !== id));
+  },[todos]);
+
+  const onToggle = useCallback(id =>{
+    setTodos(todos.map(todo => todo.id === id? {...todo,checked: !todo.checked} : todo));
+                                                // id번째 todo의 모든 값을 가지고와서 checked의 속성값만 기존 checked 값의 반대로 바꿈 
+  },[todos])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoTemplate>
+        <TodoInsert onInsert = {onInsert}/>
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+      </TodoTemplate>
+      {/* 컴포넌트도 시작/종료 태그처럼 작성이 가능하다 > 그 안에 내용을 집어 넣을수있음. 텍스트대신 컴포넌트를 넣으면 컴포넌트가 나옴. 이 컴포넌트는 템플릿의 자식요소로 들어감.(헷갈리면 개발자 찍어보기) */}
     </div>
   );
 }
 
 export default App;
+
